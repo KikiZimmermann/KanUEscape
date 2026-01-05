4cd7f9d0c0774b8bf88bb45f2fadf6d47ab07461
Kristina Zimmermann <kristina.zimmermann@stud.hcw.ac.at>
2026-01-05 18:43:32 +0100

    fixed missing resources

---
+++ src/main/resources/assets/tiles/game_screen.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="game_screen" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <editorsettings>
+  <export target="game_screen..tsx" format="tsx"/>
+ </editorsettings>
+ <image source="tileset.png" width="384" height="320"/>
+</tileset>
