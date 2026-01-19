e7b6a4f1c235bedaa438ef3d7a9587a197769602
Kristina Zimmermann <kristina.zimmermann@stud.hcw.ac.at>
2026-01-19 14:26:00 +0100

    gids for object interaction

--- src/main/java/at/ac/hcw/kanuescape/game/dialogue/DialogueManager.java
+++ src/main/java/at/ac/hcw/kanuescape/game/dialogue/DialogueManager.java
@@ -9,6 +9,41 @@ public class DialogueManager {
     // Speichert pro Object, wie oft schon geklickt wurde
     private final Map<String, Integer> clickCountsByType = new HashMap<>();
 
+    // 1) gid -> type
+    private final Map<Integer, String> gidToType = new HashMap<>();
+
+    public DialogueManager() {
+
+        register("bed", 85, 86, 97, 98, 109, 110);
+        register("picture_frame", 62);
+        register("window", 11, 23);
+        register("coffee_maker", 76);
+        register("sink", 64);
+        register("cutting_board", 63);
+        register("chair",  65, 53);
+        register("coffee", 75);
+        register("dog_food", 79);
+        register("tv", 83, 84);
+        register("picture", 66);
+        register("lamp", 99, 100, 88);
+        register("plant", 80, 92);
+        register("clock", 67);
+        register("telephone", 54);
+        register("shelves", 112);
+        register("couch", 104, 105, 106, 116, 117, 118);
+        register("trash", 52);
+        register("vase", 87);
+//        register("exit", );
+//        register("bathroom", );
+    }
+
+    private void register(String type, int... gids) {
+        for (int gid : gids) {
+            gidToType.put(gid, type);
+        }
+    }
+
+
     /**
      * liefert passenden Text zu Objekten UND zählt intern hoch
      */
