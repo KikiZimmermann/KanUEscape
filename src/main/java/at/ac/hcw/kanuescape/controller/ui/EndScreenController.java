481368b101af7255db45af6571d15e61ea60b5aa
SHofbauer10 <shofbauer-@bhakwien10.at>
2026-01-19 22:22:03 +0100

    end größe fix

--- src/main/java/at/ac/hcw/kanuescape/controller/ui/EndScreenController.java
+++ src/main/java/at/ac/hcw/kanuescape/controller/ui/EndScreenController.java
@@ -1,11 +1,16 @@
 package at.ac.hcw.kanuescape.controller.ui;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
 
 public class EndScreenController {
 
+    @FXML private StackPane endRoot;
+    @FXML private StackPane endBox;
+
     private Runnable onNewGame = () -> {};
     private Runnable onExit = () -> {};
+
 
     public void setCallbacks(Runnable onNewGame, Runnable onExit) {
         this.onNewGame = (onNewGame != null) ? onNewGame : () -> {};
